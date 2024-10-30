41aef825f7a2f3290ec41792ce41b67a0bfbf78e
LalehWalizadeh <lalah.walizadah2022@gmail.com>
2024-10-30 12:40:30 +0330

    last commit3

--- Backend/index.js
+++ Backend/index.js
@@ -17,7 +17,7 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use(
 	cors({
-		origin: ['http://localhost:3000'],
+		origin: ['https://task-project-management.vercel.app'],
 		methods: ['POST', 'GET', 'DELETE', 'PUT','PATCH'],
 		credentials: true,
 		allowedHeaders: ['Content-Type', 'Authorization'],
